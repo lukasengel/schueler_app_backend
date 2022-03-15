174f05b57444d7e4de8f5f28f51b42d980bd8e24
Lukas Engel <lukas.engel@protonmail.com>
2022-03-15 10:13:08 +0100

    Removed Credentials

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,20 @@
 "use strict";
 
 import axios from "axios";
-import { HtmlParser } from "./html_parser.js";
 import * as functions from "firebase-functions";
 import admin from "firebase-admin";
 admin.initializeApp();
+
+import { HtmlParser } from "./html_parser.js";
+import * as credentials from "./credentials.js"; //Contains sensitive data, therefore in Gitignore
 
 export const scheduledFunction = functions.pubsub.schedule("*/15 * * * *").onRun(fetchHtml);
 
 async function fetchHtml(context) {
     let response = await axios.get("https://schule-infoportal.de/infoscreen/?type=student&days=5&news=1&absent=&absent2=1", {
       auth: {
-        username: "*",
-        password: "*",
+        username: credentials.username,
+        password: credentials.password,
       }
     });
 
