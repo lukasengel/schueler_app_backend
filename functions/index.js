10bd1072b5e7f489398aa87ef33c447b0264c768
Lukas Engel <lukas.engel@protonmail.com>
2022-03-15 13:02:04 +0100

    Credentials

--- functions/index.js
+++ functions/index.js
@@ -6,29 +6,30 @@ import admin from "firebase-admin";
 admin.initializeApp();
 
 import { HtmlParser } from "./html_parser.js";
-import * as credentials from "./credentials.js"; //Contains sensitive data, therefore in Gitignore
 
 export const scheduledFunction = functions.pubsub.schedule("*/15 * * * *").onRun(fetchHtml);
 
 async function fetchHtml(context) {
-    let response = await axios.get("https://schule-infoportal.de/infoscreen/?type=student&days=5&news=1&absent=&absent2=1", {
-      auth: {
-        username: credentials.username,
-        password: credentials.password,
-      }
-    });
+  const credentials = (await admin.database().ref("credentials").get()).val();
 
-    let html = response.data;
-    const parser = new HtmlParser(html);
-    const ref = admin.database().ref("externalData");
+  let response = await axios.get("https://schule-infoportal.de/infoscreen/?type=student&days=5&news=1&absent=&absent2=1", {
+    auth: {
+      username: credentials.username,
+      password: credentials.password
+    }
+  });
 
-    ref.update({
-      "latestUpdate": parser.parseLatestUpdate(),
-      "latestFetch": new Date(),
-      "news": parser.parseNews(),
-      "ticker": parser.parseTicker(),
-      "substitutionTables": parser.parseSubstitutions(),
-    });
-    
+  let html = response.data;
+  const parser = new HtmlParser(html);
+  const ref = admin.database().ref("externalData");
+
+  await ref.update({
+    "latestUpdate": parser.parseLatestUpdate(),
+    "latestFetch": new Date(),
+    "news": parser.parseNews(),
+    "ticker": parser.parseTicker(),
+    "substitutionTables": parser.parseSubstitutions()
+  });
+
   return 0;
 }
